c3a61a49658b1cf9a8af4666e1dff11fc948582b
Vikman Fernandez-Castro <victor@wazuh.com>
2022-02-01 21:28:55 +0100

    Add time measurement

--- C/pi.c
+++ C/pi.c
@@ -17,6 +17,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 #define NAME            "pi"
 #define VERSION         "v1.1"
@@ -31,6 +32,7 @@ static void help() __attribute__((noreturn));
 int main(int argc, char ** argv) {
     long diameter = DEF_DIAMETER;
     double pi;
+    struct timespec t0, t1;
 
     if (argc > 1) {
         if (strcmp(argv[1], "--help") == 0) {
@@ -46,11 +48,18 @@ int main(int argc, char ** argv) {
         }
     }
 
+    clock_gettime(CLOCK_MONOTONIC, &t0);
     pi = calculate_pi(diameter);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+
+    t1.tv_nsec += 1000000000 - t0.tv_nsec;
+    t1.tv_sec += t1.tv_nsec / 1000000000 - t0.tv_sec - 1;
+    t1.tv_nsec %= 1000000000;
 
     printf("Pi (calculated) = %.16f\n", pi);
     printf("Pi (predefined) = %.16f\n", M_PI);
     printf("     difference = %.16f\n", fabs(pi - M_PI));
+    fprintf(stderr, "           time = %.10f ms\n", t1.tv_sec * 1000 + t1.tv_nsec / 1e6);
 
     return EXIT_SUCCESS;
 }
